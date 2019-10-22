057e91dd03f6558ca10a55f614f4c9a015f61a4d
muracchi1582 <masa1582nh@yahoo.co.jp>
2019-10-22 17:57:30 +0900

    js記述

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,19 +1,31 @@
+$((document).on('turbolinks:load', function(){
 
-
-$(function(){
   function buildHTML(message){
+    var image = "";
+    var content = "";
+    image = (message.image) ? `<image class="lower-message_image right__contents--bellow__box-message" src="${message.image}">`:"";
+    content = (message.content) ? `<div class="right__contents--bellow__box-message">${message.content}</div>` : "";
+
     var html = `<div class= "right__contents--bellow__box" data-message-id="${message.id}">
-                 <div class="raight__contents--bellow__box--name">${message.name}</div>
-                 <div class="right__contents--bellow__box--time">${message.created_at}</div>
+                  <div class="raight__contents--bellow__box--name">${message.name}</div>
+                  <div class="right__contents--bellow__box--time">${message.created_at}</div>
                 </div>`
+                ${ content }
+                ${ image }
     return html;
+  }
+
+  function scroll(){
+    $('.right__contents--bellow').animate({
+      scrollTop: $('.right__contents--bellow')[0].scrollHeight
+    }, 'slow','swing');
   }
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    var formData = new FormData(this);
+    var formData = new FormData($(this).get(0));
     var url = $(this).attr('action');
-    
+    if ((formData.get("message[content]").length != 0 || formData.get("message[image]").size != 0 )) {
     $.ajax({
       url: url,
       type: "POST",
@@ -32,5 +44,9 @@ $(function(){
     .fail(function(){
       alert('error');
     })
-  })
+    } else {
+    alert("値を入力してください");
+    }
+    return false;
+  }) 
 });
