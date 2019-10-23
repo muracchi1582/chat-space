ffa542128606262d52196ffbb5a8a46f115eb1d6
muracchi1582 <masa1582nh@yahoo.co.jp>
2019-10-23 16:48:34 +0900

    ignore記述と不要記述削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,23 +4,15 @@ $(function(){
       var image = "";
     
       image = (message.image) ? `<image class="lower--message__image right__contents--bellow__box-message" src="${message.image}">`:"";
-    //  content = (message.content) ? `<div class="right__contents--bellow__box-message">${message.content}</div>` : "";
 
     var html = `<div class= "right__contents--bellow__box" data--message--id="${message.id}">
                   <div class= "right__contents--bellow__box--name">${message.name}</div>
                   <div class= "right__contents--bellow__box--time">${message.created_at}</div>
                   <div class= "right__contents--bellow__box--message">${message.content}</div>
                 </div>`
-                  // {content}
-                  // {image}
     return html;
   }
 
-    function scroll(){
-      $('.right__contents--bellow').animate({
-        scrollTop: $('.right__contents--bellow')[0].scrollHeight
-      }, 'slow','swing');
-    }
 
   $('.new_message').on('submit', function(e){
     e.preventDefault();
@@ -38,9 +30,10 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.right__contents--bellow').append(html);
-      $('.form__message--post').val('');
-      // $('.form__submit').prop('disabled', false);
-      $('.form__submit')[0].reset();
+      $('.new_message')[0].reset();
+      $('.right__contents--bellow').animate({scrollTop: $('.right__contents--bellow')[0].scrollHeight}, 'fast');
+      $('.form__submit').prop('disabled', false);
+      
     })
     .fail(function(){
       alert('error');
