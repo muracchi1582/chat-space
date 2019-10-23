2a61d1f5c9bad2e1337ea6dc3202eab26153a687
muracchi1582 <masa1582nh@yahoo.co.jp>
2019-10-23 14:30:43 +0900

    画像とコメント実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,15 @@
 
 $(function(){
   function buildHTML(message){
-    //  var image = "";
-    //  var content = "";
-    //  image = (message.image) ? `<image class="lower--message__image right__contents--bellow__box-message" src="${message.image}">`:"";
+      var image = "";
+    
+      image = (message.image) ? `<image class="lower--message__image right__contents--bellow__box-message" src="${message.image}">`:"";
     //  content = (message.content) ? `<div class="right__contents--bellow__box-message">${message.content}</div>` : "";
 
     var html = `<div class= "right__contents--bellow__box" data--message--id="${message.id}">
                   <div class= "right__contents--bellow__box--name">${message.name}</div>
                   <div class= "right__contents--bellow__box--time">${message.created_at}</div>
                   <div class= "right__contents--bellow__box--message">${message.content}</div>
-                  <image class="lower--message__image right__contents--bellow__box-message" src="${message.image}">
                 </div>`
                   // {content}
                   // {image}
