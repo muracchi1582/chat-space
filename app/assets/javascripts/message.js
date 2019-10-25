8ab6ae802a527864256483bcea473b8738c578ca
muracchi1582 <masa1582nh@yahoo.co.jp>
2019-10-25 14:12:00 +0900

    message.js記述

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -15,7 +15,7 @@ $(function(){
   }
 
 
-  $('.new_message').on('submit', function(e){
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
@@ -43,82 +43,22 @@ $(function(){
 
 
 // 自動更新機能
-var buildMessageHTML = function(message) {
-  if (message.content && message.image.url) {
-    //data-idが反映されるようにしている
-    var html = '<div class="message" data-id=' + message.id + '>' +
-      '<div class="upper-message">' +
-        '<div class="upper-message__user-name">' +
-          message.user_name +
-        '</div>' +
-        '<div class="upper-message__date">' +
-          message.created_at +
-        '</div>' +
-      '</div>' +
-      '<div class="lower-message">' +
-        '<p class="lower-message__content">' +
-          message.content +
-        '</p>' +
-        '<img src="' + message.image.url + '" class="lower-message__image" >' +
-      '</div>' +
-    '</div>'
-  } else if (message.content) {
-    //同様に、data-idが反映されるようにしている
-    var html = '<div class="message" data-id=' + message.id + '>' +
-      '<div class="upper-message">' +
-        '<div class="upper-message__user-name">' +
-          message.user_name +
-        '</div>' +
-        '<div class="upper-message__date">' +
-          message.created_at +
-        '</div>' +
-      '</div>' +
-      '<div class="lower-message">' +
-        '<p class="lower-message__content">' +
-          message.content +
-        '</p>' +
-      '</div>' +
-    '</div>'
-  } else if (message.image.url) {
-    //同様に、data-idが反映されるようにしている
-    var html = '<div class="message" data-id=' + message.id + '>' +
-      '<div class="upper-message">' +
-        '<div class="upper-message__user-name">' +
-          message.user_name +
-        '</div>' +
-        '<div class="upper-message__date">' +
-          message.created_at +
-        '</div>' +
-      '</div>' +
-      '<div class="lower-message">' +
-        '<img src="' + message.image.url + '" class="lower-message__image" >' +
-      '</div>' +
-    '</div>'
-  　};
-  return html;
-　};
-
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    last_message_id = ※※※
+    var last_message_id = $(".right__contents--bellow__box:last").data("id");
     $.ajax({
-      //ルーティングで設定した通りのURLを指定
-      url: ※※※,
-      //ルーティングで設定した通りhttpメソッドをgetに指定
+      url: "api/messages",
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      //追加するHTMLの入れ物を作る
       var insertHTML = '';
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-
-      //メッセージが入ったHTMLを取得
-
-      //メッセージを追加
-
+      messages.forEach(function (message){
+        if(message.id > last_message){
+          insertHTML = buildHTML(message);
+          $('.right__contents--bellow').append(insertHTML);
+          $('.right__contents--bellow').animate({scrollTop: $('.right__contents--bellow')[0].scrollHeight}, 'fast');
+        }
     })
     .fail(function() {
       console.log('error');
